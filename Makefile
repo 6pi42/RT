0ba3565fdd40403e8d654c3236e2f0514850ea45
42Ned <emontagn@student.42.fr>
2016-04-03 13:04:44 +0200

    rt: initial commit

---
+++ Makefile
@@ -0,0 +1,56 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: amathias <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2015/12/03 11:09:29 by amathias          #+#    #+#              #
+#    Updated: 2016/04/03 12:11:27 by amathias         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+INC = /
+
+SRC =	main.c			\
+		cl_utils.c		\
+		hook.c			\
+		move.c			\
+
+OBJ = $(SRC:.c=.o)
+NAME = rt
+CC = gcc
+RM = rm -f
+CFLAGS = -Wall -Werror -Wextra
+
+LIB_PATH = ./libft/ /usr/local/lib/
+LIB_NAME = -lft -lmlx -framework OpenCL -framework OpenGL -framework AppKit
+LIB = $(addprefix -L,$(LIB_PATH))
+LFLAGS = $(LIB) $(LIB_NAME)
+
+INC_PATH = ./libft/ /usr/local/include/ /usr/X11/lib
+INC = $(addprefix -I,$(INC_PATH))
+
+all: lib $(NAME)
+
+lib:
+	make -C ./libft
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $(LFLAGS) $(OBJ) -o $@
+
+%.o: %.c
+	$(CC) $(INC) -o $@ -c $^ $(CFLAGS)
+
+debug: 
+	$(CC) $(CFLAGS) $(LFLAGS) -I./libft/ -L./libft/ -lft -g $(SRC) -o $(NAME)
+
+clean:
+	$(RM) $(OBJ)
+	make clean -C ./libft
+
+fclean: clean
+	$(RM) $(NAME)
+	make fclean -C ./libft
+
+re: fclean all
---
+++ rt.h
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rt.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/31 11:34:06 by amathias          #+#    #+#             */
+/*   Updated: 2016/04/03 12:10:16 by amathias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RT_H
+# define RT_H
+# include <OpenCL/cl.h>
+# include <mlx.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft.h"
+#include <stdio.h>
+
+typedef struct	s_sphere
+{
+	cl_float4 pos;
+	cl_float4 radius;
+	cl_float4 color;
+}				t_sphere;
+
+typedef struct	s_scene
+{
+	int nb_sphere;
+	t_sphere *sphere;
+}				t_scene;
+
+typedef struct	s_img
+{
+	void		*img;
+	char		*data;
+	int			bpp;
+	int			size_line;
+	int			endian;
+}				t_img;
+
+typedef struct	s_key
+{
+	int			up;
+	int			down;
+	int			left;
+	int			right;
+	int			mleft;
+	int			mright;
+}				t_key;
+
+typedef struct	s_env
+{
+	cl_device_id		id;
+	cl_context			context;
+	cl_command_queue	cmds;
+	cl_program			program;
+	cl_kernel			kernel;
+}				t_env;
+
+typedef struct	s_map
+{
+	void		*mlx;
+	void		*win;
+	t_env		env;
+	t_img		img;
+	t_key		key;
+	t_scene		scene;
+	int			height;
+	int			width;
+}				t_map;
+
+typedef struct	s_prog
+{
+	char		*buffer;
+	size_t		len;
+}				t_prog;
+
+void	draw(t_map *map);
+void	ocl_init(t_env *env, t_prog prog);
+t_prog	get_prog(char *file_name);
+int		key_hook(int keycode, t_map *map);
+int		loop_hook(t_map *map);
+void	move(t_map *map);
+void	init_key(t_map *map);
+int		key_press(int keycode, t_map *map);
+#endif
