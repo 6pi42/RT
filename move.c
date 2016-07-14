ddda3843c9f03e560762ba570f0f33e328b80052
Arnaud MATHIAS <amathias@e1r9p19.42.fr>
2016-07-14 17:59:26 +0200

    norme

--- move.c
+++ move.c
@@ -6,11 +6,34 @@
 /*   By: emontagn <emontagn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/03 11:04:26 by amathias          #+#    #+#             */
-/*   Updated: 2016/04/16 14:25:37 by emontagn         ###   ########.fr       */
+/*   Updated: 2016/07/14 17:45:04 by amathias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
+
+int		move2(t_map *map)
+{
+	int a;
+
+	a = 0;
+	if (map->key.left == 1)
+	{
+		map->scene.cam->origin.x += 2.0;
+		a = 1;
+	}
+	if (map->key.mleft == 1)
+	{
+		map->scene.cam->origin.z -= 2.0;
+		a = 1;
+	}
+	if (map->key.mright == 1)
+	{
+		map->scene.cam->origin.z += 2.0;
+		a = 1;
+	}
+	return (a);
+}
 
 void	move(t_map *map)
 {
@@ -32,21 +55,7 @@ void	move(t_map *map)
 		map->scene.cam->origin.x -= 2.0;
 		a = 1;
 	}
-	if (map->key.left == 1)
-	{
-		map->scene.cam->origin.x += 2.0;
-		a = 1;
-	}
-	if (map->key.mleft == 1)
-	{
-		map->scene.cam->origin.z -= 2.0;
-		a = 1;
-	}
-	if (map->key.mright == 1)
-	{
-		map->scene.cam->origin.z += 2.0;
-		a = 1;
-	}
+	a += move2(map);
 	if (a)
 		draw(map);
 }
